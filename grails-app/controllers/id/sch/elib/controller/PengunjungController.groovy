6daa7bad10f61c565eed14ac99de62946591e7b8
ThataAlya <nitha_thata@rocketmail.com>
2016-08-02 15:31:52 +0700

    Update Pengunjung.groovy PengunjungService.groovy dan PengunjungController.groovy

--- grails-app/controllers/id/sch/elib/controller/PengunjungController.groovy
+++ grails-app/controllers/id/sch/elib/controller/PengunjungController.groovy
@@ -1,6 +1,42 @@
 package id.sch.elib.controller
 
+import grails.converters.JSON;
+import id.sch.elib.model.Pengunjung;
+import id.sch.services.PengunjungService;
+
 class PengunjungController {
 
-    def index() { }
+    static allowedMethods = [index: "GET", save: "POST", update: "PUT", delete: ["DELETE"], search: "POST", show: "GET"]
+    def PengunjungService
+
+    def index() {
+        render PengunjungService.fetchList() as JSON
+    }
+
+    def save() {
+        def output = ["message": "success"]
+
+        if (!PengunjungService.save(request.JSON)) {
+            output = ["message": "failed"]
+        }
+
+        render output as JSON
+    }
+
+    def update() {
+        def output = ["message": "success"]
+
+        if (!PengunjungService.update(request.JSON)) {
+            output = ["message": "failed"]
+        }
+
+        render output as JSON
+    }
+
+    def show() {
+        def id = params.id
+        Pengunjung pengunjung = Pengunjung.findById(id.toLong())
+
+        render pengunjung as JSON
+    }
 }
